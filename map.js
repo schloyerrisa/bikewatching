1c8777839c6978a5d0cc6908de9ee65dcc097933
Risa Schloyer <rschloyer@ucsd.edu>
2026-05-13 14:05:11 -0700

    Update map.js

--- map.js
+++ map.js
@@ -33,4 +33,20 @@ map.on('load', async () => {
       'line-opacity': 0.4,
     },
   });
+
+    map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
 });

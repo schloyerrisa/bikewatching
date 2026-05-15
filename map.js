54fa7f983e603651b9a824cc70b42067ad3fc04d
Risa Schloyer <rschloyer@ucsd.edu>
2026-05-15 15:48:55 -0700

    adding traffic

--- map.js
+++ map.js
@@ -79,17 +79,61 @@ map.on('load', async () => {
   let stations = jsonData.data.stations;
   console.log('Stations Array:', stations);
 
-  // Append circles to the SVG for each station
-  const circles = svg
-    .selectAll('circle')
-    .data(stations)
-    .enter()
-    .append('circle')
-    .attr('r', 5) // Radius of the circle
-    .attr('fill', 'steelblue') // Circle fill color
-    .attr('stroke', 'white') // Circle border color
-    .attr('stroke-width', 1) // Circle border thickness
-    .attr('opacity', 0.8); // Circle opacity
+  const trips = await d3.csv(
+  'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv'
+);
+
+console.log('Trips Array:', trips);
+
+const departures = d3.rollup(
+  trips,
+  (v) => v.length,
+  (d) => d.start_station_id,
+);
+
+const arrivals = d3.rollup(
+  trips,
+  (v) => v.length,
+  (d) => d.end_station_id,
+);
+
+stations = stations.map((station) => {
+  let id = station.short_name;
+
+  station.arrivals = arrivals.get(id) ?? 0;
+  station.departures = departures.get(id) ?? 0;
+  station.totalTraffic = station.arrivals + station.departures;
+
+  return station;
+});
+
+console.log('Stations with traffic:', stations);
+
+const radiusScale = d3
+  .scaleSqrt()
+  .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+  .range([0, 25]);
+
+
+const circles = svg
+  .selectAll('circle')
+  .data(stations)
+  .enter()
+  .append('circle')
+  .attr('r', (d) => radiusScale(d.totalTraffic))
+  .attr('fill', 'steelblue')
+  .attr('stroke', 'white')
+  .attr('stroke-width', 1)
+  .attr('opacity', 0.8)
+  .each(function (d) {
+    // Add <title> for browser tooltips
+    d3.select(this)
+      .append('title')
+      .text(
+        `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+      );
+  });
+
 
   // Function to update circle positions when the map moves/zooms
   function updatePositions() {
